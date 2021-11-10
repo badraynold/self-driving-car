b8c32d5dd09e63ee826f9450dbfa142a675e55d7
Rafał Łukawski <rs.lukawski@gmail.com>
2021-11-10 10:15:23 +0100

    ai2 update

--- ai2.py
+++ ai2.py
@@ -2,3 +2,42 @@
 
 # libraries
 
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+class Network(nn.Module):
+    def __init__(self, input_size, ):
+        super(Network,self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action 
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
+    def forward(self, state):
+        X = F.relu(self.fc1(state))
+        q_values = self.fc2(X)
+        return q_values
+    
+# Implementing Experience Replay
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+            
+            
+            
+            
+        
